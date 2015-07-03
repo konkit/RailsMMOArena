933f6f724087d837384d6d577c4dc37c5d959848
Łukasz Tenerowicz <konkit@gmail.com>
2015-07-03 22:31:09 +0200

    Added validations to item and spell

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ActiveRecord::Base
   has_and_belongs_to_many :players
+
+  validates :name, :attack_bonus, :defence_bonus, :coin_cost, :required_level, presence: true
+
+  validates :name, blank: false
+
+  validates :attack_bonus, :defence_bonus, :coin_cost, :required_level,
+            numericality: {
+                only_integer: true
+            }
+
+  validates :coin_cost, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :required_level, numericality: { greater_than_or_equal_to: 1 }
 end
--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,3 +1,17 @@
 class Spell < ActiveRecord::Base
   has_and_belongs_to_many :players
+
+  validates :name, :damage, :prefab_type, :cooldown_time, :coin_cost, :required_level,
+            presence: true
+
+  validates :name, blank: false
+
+  validates :damage, :prefab_type, :cooldown_time, :coin_cost, :required_level,
+            numericality: {
+                only_integer: true
+            }
+
+  validates :required_level, numericality: { greater_than_or_equal_to: 1 }
+
+  validates :damage, :prefab_type, :cooldown_time, :coin_cost, numericality: { greater_than_or_equal_to: 0 }
 end

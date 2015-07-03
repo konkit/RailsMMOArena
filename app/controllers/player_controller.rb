88bf3fe4e7f44670d68a773b9e9bd5a0c13bedfb
Łukasz Tenerowicz <konkit@gmail.com>
2015-07-03 20:49:49 +0200

    Sketch of player controller

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,4 +1,11 @@
 class PlayerController < ApplicationController
-  def show_with_items_and_spells
+
+  def index
+    @player = Player.find(params[:id])
   end
+
+  def show_with_items_and_spells
+    render json: Player.find(params[:id]).as_json(:include => [:items, :spells])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'player/index/:id' => 'player#index'
   get 'player/show_with_items_and_spells'
 
   # The priority is based upon order of creation: first created -> highest priority.
